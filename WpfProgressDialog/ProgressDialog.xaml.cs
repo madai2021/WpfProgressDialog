031a0319a6604d2826c95064df17d7e620f21e7b
madai21 <>
2021-09-24 00:23:28 +0900

    First Commit

---
+++ WpfProgressDialog/ProgressDialog.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfProgressDialog
+{
+    /// <summary>
+    /// ProgressDialog.xaml の相互作用ロジック
+    /// </summary>
+    public partial class ProgressDialog : Window
+    {
+        [DllImport("user32.dll")]
+        private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll")]
+        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        const int GWL_STYLE = -16;
+        const int WS_SYSMENU = 0x80000;
+
+        private BackgroundWorker worker = new BackgroundWorker();
+
+        private Action action;
+
+        private CancellationTokenSource cancelToken;
+
+        private bool isCanceled = false;
+        public bool IsCanceled
+        {
+            get
+            {
+                return isCanceled;
+            }
+        }
+
+
+        public ProgressDialog(object context, Action action, CancellationTokenSource cancelToken)
+        {
+            InitializeComponent();
+
+            DataContext = context;
+            this.action = action;
+            this.cancelToken = cancelToken;
+
+            worker.DoWork += DoWork;
+            worker.RunWorkerCompleted += RunWorkerCompleted;
+            worker.RunWorkerAsync();
+        }
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+            IntPtr handle = new WindowInteropHelper(this).Handle;
+            int style = GetWindowLong(handle, GWL_STYLE);
+            style = style & ~WS_SYSMENU;
+            SetWindowLong(handle, GWL_STYLE, style);
+        }
+
+        private void DoWork(object sender, DoWorkEventArgs e)
+        {
+            if(action == null)
+            {
+                return;
+            }
+
+            Task task = Task.Factory.StartNew((obj) =>
+            {
+                action.Invoke();
+            }, cancelToken);
+
+            task.Wait();
+        }
+
+        private void RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            cancelToken.Cancel();
+            isCanceled = true;
+        }
+    }
+}
---
+++ WpfProgressDialog/ViewModel.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+
+namespace WpfProgressDialog
+{
+    class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private string prgTitle = "Progress";
+        public string PrgTitle
+        {
+            get
+            {
+                return prgTitle;
+            }
+            set
+            {
+                prgTitle = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private string prgStatus = "処理実行中";
+        public string PrgStatus
+        {
+            get
+            {
+                return prgStatus;
+            }
+            set
+            {
+                prgStatus = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private int prgMin = 1;
+        public int PrgMin
+        {
+            get
+            {
+                return prgMin;
+            }
+            set
+            {
+                prgMin = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private int prgMax = 100;
+        public int PrgMax
+        {
+            get
+            {
+                return prgMax;
+            }
+            set
+            {
+                prgMax = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private int prgVal = 0;
+        public int PrgVal
+        {
+            get
+            {
+                return prgVal;
+            }
+            set
+            {
+                prgVal = value;
+                int range = (prgMax - prgMin) + 1;
+                int percent = (int)(((double)prgVal / range) * 100);
+                PrgPer = percent.ToString() + "%";
+                NotifyPropertyChanged();
+            }
+        }
+
+        private string prgPer = "0%";
+        public string PrgPer
+        {
+            get
+            {
+                return prgPer;
+            }
+            set
+            {
+                prgPer = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public ICommand ExecProgress
+        {
+            get
+            {
+                return new BaseCommand(new Action(() =>
+                {
+                    CancellationTokenSource cancelToken = new CancellationTokenSource();
+                    PrgTitle = "処理実行中";
+                    PrgVal = 0;
+                    PrgMin = 1;
+                    PrgMax = 100;
+                    ProgressDialog pd = new ProgressDialog(this, () =>
+                    {
+                        for(PrgVal = 0; PrgVal < PrgMax; PrgVal++)
+                        {
+                            if(cancelToken != null && cancelToken.IsCancellationRequested)
+                            {
+                                return;
+                            }
+                            PrgStatus = "処理" + PrgVal.ToString("000") + "を実行しています";
+                            Thread.Sleep(1000);
+                        }
+                    }, cancelToken);
+
+                    pd.ShowDialog();
+                    if (pd.IsCanceled)
+                    {
+                        MessageBox.Show("キャンセルしました", "Info", MessageBoxButton.OK);
+                    }
+                    else
+                    {
+                        MessageBox.Show("完了しました", "Info", MessageBoxButton.OK);
+                    }
+                }));
+            }
+        }
+    }
+}
